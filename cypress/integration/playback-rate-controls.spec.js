65eeddd4d3f7b0b38b97437700732a209f28f478
Paweł Białaszczyk <pawelbialaszczyk@gmail.com>
2022-04-01 23:27:38 +0200

    Intercept external request in Cypress

--- cypress/integration/playback-rate-controls.spec.js
+++ cypress/integration/playback-rate-controls.spec.js
@@ -11,64 +11,60 @@ describe('Playback Rate Controls', () => {
     cy.buildBookmarklet('./src/playback-rate-controls.js').then(b => bookmarklet = b);
   });
 
-  [
-    'https://www.infoq.com/presentations/rxjs-front-end/',
-  ].forEach(url => {
-
-    context(url, () => {
-
-      beforeEach(() => {
-        cy.visit(url);
-        cy.runBookmarklet(bookmarklet);
-        media().then($media => $media.get(0).play());
-      });
-
-      it('should display controls', () => {
-        controls().should('be.visible');
-      });
-
-      it('should be able to decrease playback rate', () => {
-        media().should($media => {
-          expect($media.get(0).playbackRate).to.equal(1);
-        });
-        controls().should('contain', '1.0');
-        controls().contains('Slower').click();
-        media().should($media => {
-          expect(+$media.get(0).playbackRate.toFixed(1)).to.equal(0.9);
-        });
-        controls().should('contain', '0.9');
-        controls().contains('Slower').click();
-        media().should($media => {
-          expect(+$media.get(0).playbackRate.toFixed(1)).to.equal(0.8);
-        });
-        controls().should('contain', '0.8');
-      });
-
-      it('should be able to increase playback rate', () => {
-        media().should($media => {
-          expect($media.get(0).playbackRate).to.equal(1);
-        });
-        controls().should('contain', '1.0');
-        controls().contains('Faster').click();
-        media().should($media => {
-          expect(+$media.get(0).playbackRate.toFixed(1)).to.equal(1.1);
-        });
-        controls().should('contain', '1.1');
-        controls().contains('Faster').click();
-        media().should($media => {
-          expect(+$media.get(0).playbackRate.toFixed(1)).to.equal(1.2);
-        });
-        controls().should('contain', '1.2');
-      });
-
-      it('should hide controls while in fullscreen mode', () => {
-        cy.toggleFullscreen(true);
-        controls().should('exist').and('not.be.visible');
-        cy.toggleFullscreen(false);
-        controls().should('exist').and('be.visible');
-      });
-
+  beforeEach(() => {
+    cy.intercept('GET', '/', {
+      statusCode: 200,
+      headers: { 'Content-Type': 'text/html' },
+      body: '<audio controls src="https://www.w3schools.com/html/horse.ogg">',
     });
-
+    cy.visit('/');
+    cy.runBookmarklet(bookmarklet);
+    media().then($media => $media.get(0).play());
   });
+
+  it('should display controls', () => {
+    controls().should('be.visible');
+  });
+
+  it('should be able to decrease playback rate', () => {
+    media().should($media => {
+      expect($media.get(0).playbackRate).to.equal(1);
+    });
+    controls().should('contain', '1.0');
+    controls().contains('Slower').click();
+    media().should($media => {
+      expect(+$media.get(0).playbackRate.toFixed(1)).to.equal(0.9);
+    });
+    controls().should('contain', '0.9');
+    controls().contains('Slower').click();
+    media().should($media => {
+      expect(+$media.get(0).playbackRate.toFixed(1)).to.equal(0.8);
+    });
+    controls().should('contain', '0.8');
+  });
+
+  it('should be able to increase playback rate', () => {
+    media().should($media => {
+      expect($media.get(0).playbackRate).to.equal(1);
+    });
+    controls().should('contain', '1.0');
+    controls().contains('Faster').click();
+    media().should($media => {
+      expect(+$media.get(0).playbackRate.toFixed(1)).to.equal(1.1);
+    });
+    controls().should('contain', '1.1');
+    controls().contains('Faster').click();
+    media().should($media => {
+      expect(+$media.get(0).playbackRate.toFixed(1)).to.equal(1.2);
+    });
+    controls().should('contain', '1.2');
+  });
+
+  it('should hide controls while in fullscreen mode', () => {
+    cy.toggleFullscreen(true);
+    controls().should('exist').and('not.be.visible');
+    cy.toggleFullscreen(false);
+    controls().should('exist').and('be.visible');
+  });
+
 });
