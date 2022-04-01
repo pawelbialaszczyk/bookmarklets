48dee6ba0b0b7b013cdd17e68a714bee7af6c118
Paweł Białaszczyk <pawelbialaszczyk@gmail.com>
2022-04-01 23:45:27 +0200

    Restore default Cypress plugin index file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,10 +1,1 @@
-module.exports = (on, config) => {
-  on('before:browser:launch', (browser, launchOptions) => {
-    if (browser.name === 'chrome' || browser.name === 'canary') {
-      // https://peter.sh/experiments/chromium-command-line-switches/
-      launchOptions.args.push('--mute-audio');
-    }
-
-    return launchOptions;
-  })
-}
+module.exports = (on, config) => { };
