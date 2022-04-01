962cc09c7a23be2a99701d36fc9f218131338eab
Paweł Białaszczyk <pawelbialaszczyk@gmail.com>
2022-04-01 22:47:04 +0200

    Update Cypress to v9

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const bookmarkleter = require('bookmarkleter');
 
-const [sourcePath] = JSON.parse(process.env.npm_config_argv).remain;
+const [sourcePath] = process.argv.slice(2);
 const source = fs.readFileSync(sourcePath, 'utf8');
 const bookmarklet = bookmarkleter(source, {
   iife: true,
